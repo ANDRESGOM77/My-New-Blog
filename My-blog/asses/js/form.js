f1680b006c38c5631babb3cbccc770a3dd307282
Andres Gomez <andregom2009@gmail.com>
2024-03-27 18:24:19 +1100

    creating the method for the form

--- My-blog/asses/js/form.js
+++ My-blog/asses/js/form.js
@@ -1,24 +1,23 @@
-document.getElementById("miFormulario").addEventListener("submit", function(event) {
-    event.preventDefault(); // Evitar el envío del formulario
+//creating method to save the new data in the list that is in the local storage
+document.getElementById("Form").addEventListener('submit', function(event) {
 
-    var nombre = document.getElementById("nombre").value;
-    var email = document.getElementById("email").value;
-    var comentario = document.getElementById("comentario").value;
+    event.preventDefault();
 
-    // Obtener todos los datos almacenados actualmente
-    var datosGuardados = JSON.parse(localStorage.getItem("datos") || "[]");
+    let username=document.getElementById("Username").value;
+    let title=document.getElementById("Title").value;
+    let comment=document.getElementById("Content").value;
+    //Creating new object
+    const NewPost ={
+        username: username,
+        title: title,
+        comment: comment
+        };
+    let postsList = JSON.parse(localStorage.getItem('posts')) || [];
+    postsList.push(NewPost);
+    //Save the updated data to local storage
+    localStorage.setItem('posts', JSON.stringify(postsList));   
+    // Redirect to New page blog
+    window.location.href="blog.html";
 
-    // Agregar los nuevos datos
-    var nuevoDato = {
-        nombre: nombre,
-        email: email,
-        comentario: comentario
-    };
-    datosGuardados.push(nuevoDato);
-
-    // Guardar los datos actualizados en el almacenamiento local
-    localStorage.setItem("datos", JSON.stringify(datosGuardados));
-
-    // Redirigir a la página de visualización
-    window.location.href = "visualizar.html";
-})
+    console.log(postsList);
+});
