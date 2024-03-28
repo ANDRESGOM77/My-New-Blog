508aa6b2e39405e4b0f08fe6411384486d9b40dd
Andres Gomez <andregom2009@gmail.com>
2024-03-28 17:21:08 +1100

    changing blog.js

--- My-blog/asses/js/blog.js
+++ My-blog/asses/js/blog.js
@@ -1,20 +1,24 @@
+//CALL THE POSTSLIST FROM THE LOCAL STORAGE
 let postsList = JSON.parse(localStorage.getItem('postsList')) || [];
 let Container = document.getElementById('Container');
 
+//CREATING A FUNCTION TO ADD THE CARD WITH THE ELEMENTS
 postsList.forEach(function (formData) {
     let card = document.createElement('div');
     card.className = 'card';
-
-    let username = document.createElement('h2')
-    let title = document.createElement('h3')
-    let content = document.createElement('p')
-
-    content.className = 'content'
-
-    username.textContent = 'Username: ' + formData.username
-    title.textContent = 'Title: ' + formData.title
-    content.textContent = formData.Content
-
-    card.append(username, title, content)
+//ADDING SOME PROPERTIES
+    let username = document.createElement('h2');
+    let title = document.createElement('h3');
+    let content = document.createElement('p');
+//CREATING CLASSES FOR CSS
+    username.className = 'Username';
+    title.className = 'Title';
+    content.className = 'content';
+//SHOWING IN SCREEN THE VALUE OF THE VARIABLE
+    username.textContent = 'Username: ' + formData.username;
+    title.textContent = 'Title: ' + formData.title;
+    content.textContent = formData.Content;
+//ADDING EVERYTHING TO CARD AND THEN TO CONTAINER
+    card.append(username, title, content);
     Container.append(card);
 });
